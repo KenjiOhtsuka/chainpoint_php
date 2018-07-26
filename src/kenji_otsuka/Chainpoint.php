01c1b998533f3d8e89affc8e0266559aff8ae9c5
Kenji Ohtsuka <kok.fdcm@gmail.com>
2018-07-26 12:21:32 +0900

    update source

---
+++ src/kenji_otsuka/Chainpoint.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace KenjiOtsuka;
+
+class Cainpoint {
+  const LIST = [
+    "https://a.chainpoint.org/nodes/random",
+    "https://b.chainpoint.org/nodes/random",
+    "https://c.chainpoint.org/nodes/random"
+  ];
+
+  function pickupServer() {
+
+  }
+
+  function submit(string $hash) {
+
+  }
+
+  function getProof(string $hashIdNode) {
+
+  }
+
+  function verify(string $proof) {
+
+  }
+}
